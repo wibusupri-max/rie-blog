8d1d30a5e355b9c443f4af0b09a69059eb945202
wibusupri-max <wibusupri@gmail.com>
2026-06-29 21:32:49 +0700

    Initial setup: Add Article model

---
+++ backend/models/Article.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ['teknologi', 'anime', 'lainnya'],
+    default: 'lainnya'
+  },
+  author: {
+    type: String,
+    default: 'Admin'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Article', articleSchema);
